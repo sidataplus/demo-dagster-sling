46aa3f1c5c0e0078f65c953d0bbc71a178b65787
Max Adulyanukosol <na399@unc.edu>
2023-11-30 22:08:27 -0500

    Update __init__.py

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -15,15 +15,16 @@ all_assets_job = define_asset_job(name="all_assets_job", selection=AssetSelectio
 daily_refresh_schedule = ScheduleDefinition(
     job=all_assets_job,
     # cron_schedule="0 0 * * *", # every day at midnight
+    # cron_schedule="0 1 * * *", # every day at 1am
 	cron_schedule="* * * * *", # every minute
 )
 
 defs = Definitions(
-    assets=all_assets,
-    resources={
+	resources={
         "sling_parquet": resources.sling_parquet,
         "sling_csv": resources.sling_csv,
     },
+    assets=all_assets,
 	jobs=[all_assets_job],
     schedules=[daily_refresh_schedule],
 )
